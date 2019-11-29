103b1f9102c9bd9cd17b37c5763acc436783e5ba
jltsao88 <jltsao88@gmail.com>
2019-11-29 10:39:16 -0500

    added search functionality

--- my_app/views.py
+++ my_app/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'base.html')
+    return render(request, template_name='base.html')
+
+def new_search(request):
+    return render(request, 'my_app/new_search.html')
